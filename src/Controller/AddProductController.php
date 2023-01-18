524ece1e8f48c5d5957276296d5dae1b2a02462f
Wagner Kaba <bom.link+github@gmail.com>
2023-01-18 07:16:58 -0300

    Refactor ObjectFactory to simplify code

--- src/Controller/AddProductController.php
+++ src/Controller/AddProductController.php
@@ -4,6 +4,9 @@ namespace WKaba\ProductPage\Controller;
 
 use Doctrine\ORM\EntityManager;
 use WKaba\ProductPage\Entity\DVD;
+use WKaba\ProductPage\Entity\Book;
+use WKaba\ProductPage\Entity\Furniture;
+use WKaba\ProductPage\Factory\ObjectFactory;
 use WKaba\ProductPage\Service\ProductService;
 
 class AddProductController implements Controller
@@ -18,10 +21,14 @@ class AddProductController implements Controller
     public function run()
     {
         $request = file_get_contents('php://input');
-        $jsonData = json_decode($request, true);
-        $dvd = new DVD($jsonData['sku'],$jsonData['name'],$jsonData['price'],$jsonData['size']);
+        $data = json_decode($request, true);
+
+        $factory = new ObjectFactory();
+
+        $product = $factory->createObject($data['type'], $data);
+
         $productService = new ProductService($this->entityManager);
-        $productService->addProduct($dvd);
+        $productService->addProduct($product);
 
         http_response_code(201);
     }
--- src/Factory/ObjectFactory.php
+++ src/Factory/ObjectFactory.php
@@ -2,7 +2,28 @@
 
 namespace WKaba\ProductPage\Factory;
 
+use Exception;
+use ReflectionClass;
+
 class ObjectFactory
 {
+    private $config = [
+        "book" => "WKaba\ProductPage\Entity\Book",
+        "dvd" => "WKaba\ProductPage\Entity\DVD",
+        "furniture" => "WKaba\ProductPage\Entity\Furniture"
+    ];
+
+    public function createObject($type, $data) {
+
+        if (!isset($this->config[$type])) {
+            throw new Exception("Invalid object type");
+        }
+
+        $className = $this->config[$type];
+
+        $class = new ReflectionClass($className);
+
+        return $class->newInstanceArgs($data['attributes']);
+    }
 
 }
